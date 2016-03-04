ab0bae4a65d3db309b9e3ec40a2c6a67447e2693
greduan <me@greduan.com>
2016-03-03 18:42:05 -0600

    Add on{Before,After}Destroy hooks

--- tests/integration/destroy.js
+++ tests/integration/destroy.js
@@ -21,5 +21,58 @@ module.exports = function(session) {
         });
       });
     });
+
+  });
+
+  describe('Model Destroy Hooks', function () {
+    it('Should execute beforeDestroy hooks in order', function () {
+      Model2.validations = {};
+
+      var model = new Model2();
+
+      model.on('beforeDestroy', function(next) {
+        setTimeout(function() {
+          model.property2 = 1;
+          next();
+        }, 1000);
+      });
+
+      model.on('beforeDestroy', function(next) {
+        model.property2++;
+        next();
+      });
+
+      return model.destroy().then(function(res) {
+        expect(model.property2).to.be.eql(2);
+        expect(model.id).to.be.null;
+        expect(res.id).to.be.null;
+        expect(res).is.an.instanceOf(Model2);
+      });
+    });
+
+    it('Should execute afterDestroy hooks in order', function () {
+      Model2.validations = {};
+
+      var model = new Model2();
+
+      model.on('afterDestroy', function(next) {
+        setTimeout(function() {
+          model.property2 = 1;
+          next();
+        }, 1000);
+      });
+
+      model.on('afterDestroy', function(next) {
+        model.property2++;
+        next();
+      });
+
+      return model.destroy().then(function(res) {
+        expect(model.property2).to.be.eql(2);
+        expect(model.id).to.be.null;
+        expect(res.id).to.be.null;
+        expect(res).is.an.instanceOf(Model2);
+      });
+    });
   });
 }
