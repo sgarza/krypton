566d8dca4920767c373d4a51737e7f68d40585ef
greduan <me@greduan.com>
2016-03-30 15:32:19 -0600

    Krypton.Relation will check models super tree for Krypton.Model

--- krypton/Relation.js
+++ krypton/Relation.js
@@ -1,5 +1,24 @@
 var _ = require('lodash');
 
+var loopThroughSuper = function (model) {
+  var currentModel = model;
+  var superName = Krypton.Model.className;
+  var isSubclass = false;
+
+  // While we still haven't found it to be a subclass of Krypton.Model
+  // AND it has a super class.
+  while (!isSubclass && currentModel.superClass !== null) {
+    // If we find it to be a subclass of Krypton.Model
+    if (currentModel.superClass.className === superName) {
+      isSubclass = true;
+    } else {
+      currentModel = currentModel.superClass;
+    }
+  }
+
+  return isSubclass;
+};
+
 Krypton.Relation = Class(Krypton, 'Relation')({
   prototype : {
     name : null,
@@ -15,8 +34,16 @@ Krypton.Relation = Class(Krypton, 'Relation')({
         throw new Error('Must provide an ownerModel');
       }
 
+      if (!loopThroughSuper(config.ownerModel)) {
+        throw new Error('ownerModel is not (eventually) a subclass of Krypton.Model');
+      }
+
       if (!config.relatedModel) {
         throw new Error('Must provide a relatedModel');
+      }
+
+      if (!loopThroughSuper(config.relatedModel)) {
+        throw new Error('relatedModel is not (eventually) a subclass of Krypton.Model');
       }
 
       if (!config.ownerCol || !_.isString(config.ownerCol)) {
