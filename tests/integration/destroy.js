aeb5ed5b5e18f1337059a1391e141c7889084116
Sergio de la Garza <sergio@delagarza.io>
2016-08-29 21:14:40 -0500

    Refactor tests

--- tests/integration/destroy.js
+++ tests/integration/destroy.js
@@ -2,49 +2,49 @@
 /* eslint no-unused-expressions: 0 */
 
 const expect = require('chai').expect;
-const Knex = require('knex');
 const path = require('path');
 
 const truncate = require(path.join(__dirname, '..', 'truncate'));
 
 describe('Model destroy', () => {
   afterEach(() => {
-    truncate([Model1]);
+    truncate([Model1, Model2]);
   });
 
-  it('Should destroy a model', () {
-    return Model1.query().where({id : 1}).then(function(result) {
-      var model = result[0];
+  it('Should destroy a model', () => {
+    return new Model1({}).save().then(() => {
+      return Model1.query().where({ id: 1 }).then((result) => {
+        const model = result[0];
 
-      return model.destroy().then(function(res) {
-        expect(model.id).to.be.null;
-        expect(res.id).to.be.null;
-        expect(res).is.an.instanceOf(Model1);
+        return model.destroy().then((res) => {
+          expect(model.id).to.be.null;
+          expect(res.id).to.be.null;
+          expect(res).is.an.instanceOf(Model1);
+        });
       });
     });
   });
-
 });
 
-describe('Model Destroy Hooks', function () {
-  it('Should execute beforeDestroy hooks in order', function () {
+describe('Model Destroy Hooks', () => {
+  it('Should execute beforeDestroy hooks in order', () => {
     Model2.validations = {};
 
-    var model = new Model2();
+    const model = new Model2();
 
-    model.on('beforeDestroy', function(next) {
-      setTimeout(function() {
+    model.on('beforeDestroy', (next) => {
+      setTimeout(() => {
         model.property2 = 1;
         next();
       }, 1000);
     });
 
-    model.on('beforeDestroy', function(next) {
+    model.on('beforeDestroy', (next) => {
       model.property2++;
       next();
     });
 
-    return model.destroy().then(function(res) {
+    return model.destroy().then((res) => {
       expect(model.property2).to.be.eql(2);
       expect(model.id).to.be.null;
       expect(res.id).to.be.null;
@@ -52,24 +52,24 @@ describe('Model Destroy Hooks', function () {
     });
   });
 
-  it('Should execute afterDestroy hooks in order', function () {
+  it('Should execute afterDestroy hooks in order', () => {
     Model2.validations = {};
 
-    var model = new Model2();
+    const model = new Model2();
 
-    model.on('afterDestroy', function(next) {
-      setTimeout(function() {
+    model.on('afterDestroy', (next) => {
+      setTimeout(() => {
         model.property2 = 1;
         next();
       }, 1000);
     });
 
-    model.on('afterDestroy', function(next) {
+    model.on('afterDestroy', (next) => {
       model.property2++;
       next();
     });
 
-    return model.destroy().then(function(res) {
+    return model.destroy().then((res) => {
       expect(model.property2).to.be.eql(2);
       expect(model.id).to.be.null;
       expect(res.id).to.be.null;
